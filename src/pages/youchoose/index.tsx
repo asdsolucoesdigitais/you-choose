542139a97f1e3659c1f696c25d5b176790ded33e
Guilherme Amorim <guilherme.o.a.ufal@gmail.com>
2022-04-30 01:21:01 -0300

    feat: first commit

---
+++ src/pages/youchoose/index.tsx
@@ -0,0 +1,197 @@
+import StyleYouChoose from "./YouChoose.module.scss";
+
+export default function YouChoose() {
+    return (
+        <div className={StyleYouChoose.container}>
+            <div className={StyleYouChoose.box}>
+                <div className={StyleYouChoose.box__movie}>
+                    <div className={StyleYouChoose.box__movie__poster}></div>
+                </div>
+            </div>
+            <div className={StyleYouChoose.box}>
+                <div className={StyleYouChoose.box__movie}>
+                    <div className={StyleYouChoose.box__movie__poster}></div>
+                </div>
+            </div>
+            <div className={StyleYouChoose.box}>
+                <div className={StyleYouChoose.box__ranking}>
+                    <div className={StyleYouChoose.box__ranking__item}>
+                        <div
+                            className={
+                                StyleYouChoose.box__ranking__item__poster_mini
+                            }
+                        >
+                            IMG
+                        </div>
+                        <div
+                            className={StyleYouChoose.box__ranking__item__title}
+                        >
+                            <h1
+                                className={
+                                    StyleYouChoose.box__ranking__item__title__h1
+                                }
+                            >
+                                Title
+                            </h1>
+                        </div>
+                        <div
+                            className={StyleYouChoose.box__ranking__item__score}
+                        >
+                            SCORE
+                        </div>
+                    </div>
+                    <div className={StyleYouChoose.box__ranking__item}>
+                        <div
+                            className={
+                                StyleYouChoose.box__ranking__item__poster_mini
+                            }
+                        >
+                            IMG
+                        </div>
+                        <div
+                            className={StyleYouChoose.box__ranking__item__title}
+                        >
+                            <h1
+                                className={
+                                    StyleYouChoose.box__ranking__item__title__h1
+                                }
+                            >
+                                Title
+                            </h1>
+                        </div>
+                        <div
+                            className={StyleYouChoose.box__ranking__item__score}
+                        >
+                            SCORE
+                        </div>
+                    </div>
+                    <div className={StyleYouChoose.box__ranking__item}>
+                        <div
+                            className={
+                                StyleYouChoose.box__ranking__item__poster_mini
+                            }
+                        >
+                            IMG
+                        </div>
+                        <div
+                            className={StyleYouChoose.box__ranking__item__title}
+                        >
+                            <h1
+                                className={
+                                    StyleYouChoose.box__ranking__item__title__h1
+                                }
+                            >
+                                Title
+                            </h1>
+                        </div>
+                        <div
+                            className={StyleYouChoose.box__ranking__item__score}
+                        >
+                            SCORE
+                        </div>
+                    </div>
+                    <div className={StyleYouChoose.box__ranking__item}>
+                        <div
+                            className={
+                                StyleYouChoose.box__ranking__item__poster_mini
+                            }
+                        >
+                            IMG
+                        </div>
+                        <div
+                            className={StyleYouChoose.box__ranking__item__title}
+                        >
+                            <h1
+                                className={
+                                    StyleYouChoose.box__ranking__item__title__h1
+                                }
+                            >
+                                Title
+                            </h1>
+                        </div>
+                        <div
+                            className={StyleYouChoose.box__ranking__item__score}
+                        >
+                            SCORE
+                        </div>
+                    </div>
+                    <div className={StyleYouChoose.box__ranking__item}>
+                        <div
+                            className={
+                                StyleYouChoose.box__ranking__item__poster_mini
+                            }
+                        >
+                            IMG
+                        </div>
+                        <div
+                            className={StyleYouChoose.box__ranking__item__title}
+                        >
+                            <h1
+                                className={
+                                    StyleYouChoose.box__ranking__item__title__h1
+                                }
+                            >
+                                Title
+                            </h1>
+                        </div>
+                        <div
+                            className={StyleYouChoose.box__ranking__item__score}
+                        >
+                            SCORE
+                        </div>
+                    </div>
+                    <div className={StyleYouChoose.box__ranking__item}>
+                        <div
+                            className={
+                                StyleYouChoose.box__ranking__item__poster_mini
+                            }
+                        >
+                            IMG
+                        </div>
+                        <div
+                            className={StyleYouChoose.box__ranking__item__title}
+                        >
+                            <h1
+                                className={
+                                    StyleYouChoose.box__ranking__item__title__h1
+                                }
+                            >
+                                Title
+                            </h1>
+                        </div>
+                        <div
+                            className={StyleYouChoose.box__ranking__item__score}
+                        >
+                            SCORE
+                        </div>
+                    </div>
+                    <div className={StyleYouChoose.box__ranking__item}>
+                        <div
+                            className={
+                                StyleYouChoose.box__ranking__item__poster_mini
+                            }
+                        >
+                            IMG
+                        </div>
+                        <div
+                            className={StyleYouChoose.box__ranking__item__title}
+                        >
+                            <h1
+                                className={
+                                    StyleYouChoose.box__ranking__item__title__h1
+                                }
+                            >
+                                Title
+                            </h1>
+                        </div>
+                        <div
+                            className={StyleYouChoose.box__ranking__item__score}
+                        >
+                            SCORE
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
